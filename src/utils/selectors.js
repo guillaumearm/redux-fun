5fc63a71e217c8ab822b6f89ed45dd7e20a49ea1
Guillaume ARM <trapcodien@hotmail.fr>
2018-01-12 20:51:05 +0100

    Clarify bindSelectors syntax

--- src/utils/selectors.js
+++ src/utils/selectors.js
@@ -1,7 +1,9 @@
 const mapValues = require('lodash.mapvalues');
 
 const bindSelectors = (selectors, getState) => (
-  mapValues(selectors, (selector) => (...args) => selector(getState(), ...args))
+  mapValues(selectors, (selector) => (
+    (...args) => selector(getState(), ...args)
+  ))
 )
 
 module.exports = {
